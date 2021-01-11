676c8f7baded2ceddca8a701b8974e7b0b9ba6ff
Gnaphron <gnaphron@gmail.com>
2021-01-10 19:45:09 -0800

    updates

--- template/gstrust-http-template/main/src/main.rs
+++ template/gstrust-http-template/main/src/main.rs
@@ -1,25 +1,52 @@
-use std::convert::Infallible;
+#![deny(warnings)]
 
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Error, Request, Response, Server};
 
-async fn handler_service(req: Request<Body>) -> Result<Response<Body>, Infallible>{
-	Ok(handler::handle(req).await)
+fn main() {
+    // Configure a runtime that runs everything on the current thread
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .expect("build runtime");
+
+    // Combine it with a `LocalSet,  which means it can spawn !Send futures...
+    let local = tokio::task::LocalSet::new();
+    local.block_on(&rt, run());
 }
 
-#[tokio::main]
-pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let make_svc = make_service_fn(|_conn| {
-        async { Ok::<_, Infallible>(service_fn(handler_service)) }
-    });
+async fn handler_fn(_req: Request<Body>) -> Result<Response<Body>, Error> {
+    Ok(handler::handle(_req).await)
+}
 
+async fn run() {
     let addr = ([0, 0, 0, 0], 3000).into();
 
-    let server = Server::bind(&addr).serve(make_svc);
+    let make_service = make_service_fn(|_conn| {
+        async { Ok::<_, Error>(service_fn(handler_fn)) }
+    });
+
+    let server = Server::bind(&addr).executor(LocalExec).serve(make_service);
 
     println!("Listening on http://{}", addr);
 
-    server.await?;
+    // The server would block on current thread to await !Send futures.
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
 
-    Ok(())
+// Since the Server needs to spawn some background tasks, we needed
+// to configure an Executor that can spawn !Send futures...
+#[derive(Clone, Copy, Debug)]
+struct LocalExec;
+
+impl<F> hyper::rt::Executor<F> for LocalExec
+where
+    F: std::future::Future + 'static, // not requiring `Send`
+{
+    fn execute(&self, fut: F) {
+        // This will spawn into the currently running `LocalSet`.
+        tokio::task::spawn_local(fut);
+    }
 }
