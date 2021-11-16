2fe2100d6680c297b63df98dbbf98845ac15bb8d
Gnaphron <gnaphron@gmail.com>
2021-11-15 19:28:53 -0800

    Add Gstreamer Python template

---
+++ template/gstpython-http-template/function/handler.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst, GLib
+
+def handle(req):
+    """handle a request to the function
+    Args:
+        req (str): request body
+    """
+    Gst.init(None)
+    version = Gst.version_string()
+
+    return f"Gstreamer version: {version} on 🐍"
+
+
+if __name__ == "__main__":
+    req = """
+    Some data
+    """
+    
+    res = handle(req)
+    print(res)
+
